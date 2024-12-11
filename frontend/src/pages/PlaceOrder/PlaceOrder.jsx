2bd8d4e774ec52e26198dcbf8591c7101513737c
Acer <sirajmuhd.123@gmail.com>
2024-12-11 16:59:21 +0530

    cart-section

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
+
+  const {getTotalCartAmount} = useContext(StoreContext)
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={placeOrder} className='place-order'>
+      <div className="place-order-left">
+        <p className='title'>Delivery Information</p>
+        <div className="multi-fields">
+          <input required name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='First name'/>
+          <input required name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder='Last name'/>
+        </div>
+        <input required name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='E-mail Address'/>
+        <input required name='street' onChange={onChangeHandler} value={data.street} type="text"  placeholder='Street'/>
+        <div className="multi-fields">
+          <input required name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City'/>
+          <input required name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='State'/>
+        </div>
+        <div className="multi-fields">
+          <input name='zipcode' onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='Zip code'/>
+          <input required name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country'/>
+        </div>
+        <input required name='phone' onChange={onChangeHandler} value={data.phone} type="text"  placeholder='Phone'/>
+      </div>
+      <div className="place-order-right">
+      <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+          <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+          </div>
+          </div>
+          <button type='submit'>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
   )
 }
 
