4da686d60007bb3f60a353a97d6fbc09d305d13f
Muhammed Siraj <sirajmuhd.123@gmail.com>
2024-12-19 17:57:03 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontend
 const placeOrder = async (req,res) =>{
 
-    const frontend_url = "https://food-ordering-website-frontend-w8xw.onrender.com";
+    const frontend_url = "https://food-ordering-website-frontend-atkr.onrender.com";
 
     try {
         const newOrder = new orderModel({
